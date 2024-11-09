508756d29054abd41b2a6a5f839d2995e6a50de4
Francisco Valerio <119526154+Drago1002@users.noreply.github.com>
2024-11-08 22:07:31 -0600

    Update login.js

--- login.js
+++ login.js
@@ -1,12 +1,13 @@
-//funiones del login
 document.getElementById("login-form").addEventListener("submit", function(event) {
     event.preventDefault(); 
     
     const username = document.getElementById("username").value;
     const password = document.getElementById("password").value;
 
-    if (username === "fco" && password === "abc123") {
-        window.location.href = "index.html";
+    if (username === "Fran10" && password === "abc123") {
+        window.location.href = "index.html";  
+    } else if (username === "Admin" && password === "admin123") {
+        window.location.href = "admin.html"; 
     } else {
         alert("Datos incorrectos. Intenta nuevamente.");
     }
